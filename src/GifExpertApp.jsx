26d74396cda9b2f4a66cf59344cd1af6ecaad26e
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2024-10-05 12:25:26 -0300

    feature: Create GifExpertApp component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,6 +2,11 @@ const GifExpertApp = () => {
   return (
     <>
         <h1>Gif Expert App</h1>
+        
+        {/* Input (Buscador) */}
+
+        {/* Listado de Gif */}
+          {/* Gif items */}
     </>
   );
 };
