559687d3d25ae5c41c7c2ba1fa17b67ae4de8abf
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2024-10-05 14:50:18 -0300

    feature: Categories list

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Otro']);
+
   return (
     <>
         <h1>Gif Expert App</h1>
-        
+
         {/* Input (Buscador) */}
 
         {/* Listado de Gif */}
+        <ol>
+          { categories.map( (categorie) => (<li key={ categorie }>{ categorie }</li>) ) }
+        </ol>
           {/* Gif items */}
     </>
   );
