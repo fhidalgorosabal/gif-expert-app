e0695eee5fff32e029c2ff06fe8f64263a123aa3
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2024-10-06 18:31:26 -0300

    feature: Show images

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
+import GifItem from './GifItem';
 
 export const GifGrid = ({category}) => {
     const [images, setImages] = useState([]);
@@ -15,9 +16,16 @@ export const GifGrid = ({category}) => {
     return (
         <>
             <h3>{ category }</h3>
-            <ol>
-                { images.map(({id, title}) => ( <li key={ id }>{ title }</li> )) }
-            </ol>
+            <div className='card-grid'>
+                { 
+                    images.map(image => ( 
+                        <GifItem 
+                            key={ image.id } 
+                            { ...image }
+                        /> 
+                    )) 
+                }
+            </div>
         </>
     )
 };
