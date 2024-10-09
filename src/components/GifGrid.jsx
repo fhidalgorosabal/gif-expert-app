90f0b41c4a81ad5bc2c474ba624bced3455b9473
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2024-10-09 18:43:17 -0300

    fix: Import GifGrid component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
-import GifItem from './GifItem';
+import { GifItem } from './GifItem';
 
 export const GifGrid = ({category}) => {
     const [images, setImages] = useState([]);
